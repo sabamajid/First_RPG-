2eef542f28176c95a03c11e82d6f30a43f5dc31e
sabamajid <saba.majid718@gmail.com>
2023-07-14 15:22:20 +0500

    character goes to another scene so well

--- Assets/Scripts/SpawnCharacter.cs
+++ Assets/Scripts/SpawnCharacter.cs
@@ -7,17 +7,23 @@ public class SpawnCharacter : MonoBehaviour
 {
 
     public GameObject[] characterPrefabs;
-    public Transform spawnPoint;
+    public Transform[] spawnPoint;
     public Text Name;
+    private int pos;
 
     private void Start()
     {
         int[] selectedCharacter = { PlayerPrefs.GetInt("selectedCharacter1"), PlayerPrefs.GetInt("selectedCharacter2"), PlayerPrefs.GetInt("selectedCharacter3"), };
+      
 
         for (int i=0; i<selectedCharacter.Length; i++)
         {
+           
+
             GameObject prefab = characterPrefabs[selectedCharacter[i]];
-            GameObject clone = Instantiate(prefab, spawnPoint.position, Quaternion.identity);
+            GameObject clone = Instantiate(prefab, spawnPoint[pos].position, Quaternion.identity);
+            pos ++;
+
             Name.text = prefab.name;
         }
 
